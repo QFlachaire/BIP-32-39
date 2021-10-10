e38fb3aa436aa7ac72799f03712181cb529b0f5b
Quentin Flachaire <quentin.flachaire@gmail.com>
2021-10-10 18:02:36 +0200

    Bip39 Done.
    Co-authored-by: NasreddineG <NasreddineG@users.noreply.github.com>

--- Bip39.py
+++ Bip39.py
@@ -3,59 +3,121 @@ import sys
 import math
 import hashlib
 import re
+import binascii
+import unicodedata
 
+"""            """
 # Test sentence
-# pudding bottom soldier breeze educate grass order oxygen pottery crystal tongue among
+# thrive call mistake crack glare sustain expand lend tip install camp patient
 
-def get_seed():
-    seed = []
-    print(f"sentence or word by word (word) ?")
-    rep = input()
+def check_phrase(twelve_eleven):
+    
+    deb = twelve_eleven[:128]
+    fin = twelve_eleven[-4:]
 
-    if rep == "word":
-        for i in range(1, 13, 1):
-            print(f"Write your word {i} :")
-            seed.append(input())
-    else:
-        print(f"Write your sentence:")
-        seed = re.sub("[^\w]", " ",  input()).split()
-    print(seed)
-    return(seed)
+    deb_hex = hex(int(deb,2))
 
-def get_rand_seed():
+    deb_byte = binascii.a2b_hex(deb_hex[2:]) # Hex to Byte
+    
+    hash_256 = hashlib.sha256(deb_byte).hexdigest() 
+    checksum = bin(int(hash_256, 16))[2:].zfill(128)[:4]
 
-    rand = os.urandom(16) 
-    entropy = bin(int.from_bytes(rand, "big"))[2:]
-    hash_256 = hashlib.sha256(entropy.encode()).digest() #hash en hexadécimal
+    if checksum == fin:
+        print("\nThe mnemonic corresponds to the BIP 39 standard\nHere is the checksum and the last 4 bit of the Hash :\n", checksum, fin)
+        return(True)
+    if checksum != fin: 
+        print("\nThe seed you entered isn't correct. Try again !\n") 
+        return(False)
 
-    four_bits = bin(int.from_bytes(hash_256, "big"))[2:6]
-    print(len(entropy))
-    print(len(four_bits))
-    twelve_eleven = entropy + four_bits
-    print(len(twelve_eleven))
+def get_phrase_from_user(phrase):
+    phrase = re.sub("[^\w]", " ",  phrase).split()
+
     with open("english.txt", "r") as f:
         wordlist = [w.strip() for w in f.readlines()]
+    
+    twelve_eleven = ""
 
-    seed = []
-    for i in range(0, 12, 1):
-        indx = int(twelve_eleven[11*i:11*(i+1)])
-        seed.append(wordlist[indx])
-    return(seed)
+    for word in phrase:
+        try:
+            index = bin(wordlist.index(word))[2:]
+        except:
+            print("\nYour word is not in BIP39 list, try again")
+            exit()
+        index = '0'*(11-len(index)) + index
+        twelve_eleven += index
+    print("\nAll your words are in the BIP39 list")
+    check_phrase(twelve_eleven)
+    return(twelve_eleven)
+
+def get_rand_phrase():
+
+    bits = 128
+    bytes = bits//8 # =16
+
+    rand = os.urandom(bytes) 
+
+    rand_hex = binascii.b2a_hex(rand) # Bytes to hex
+    print("\nHere is the random int generate with urandom :\n", int(rand_hex,16))
+
+    checksum_length = bits//32
+
+    hash_256 = hashlib.sha256(rand).hexdigest() # Hash in Hex
+
+    entropy = bin(int(rand_hex, 16))[2:].zfill(bits)
+    checksum = bin(int(hash_256, 16))[2:].zfill(bits)[:checksum_length]
+
+    twelve_eleven = entropy + checksum
+
+    check_phrase(twelve_eleven)
+    dic = []
+    with open("english.txt", "r") as f:
+        for w in f.readlines():
+            dic.append(w.strip())
+   
+    print("\nThis is all the twelve eleven bits, their integer and their words associate :")
+    wordlist = []
+    for i in range(len(twelve_eleven) // 11):
+        index_bin = twelve_eleven[11*i : 11*(i+1)]
+        index = int(index_bin,2) # In base 2
+        wordlist.append(dic[index])
+        print(i+1, index_bin, " | " , dic[index])
+
+    phrase = ' '.join(wordlist)
+    print(f"\nThis is your mnemonic phrase :\n{phrase}\n")
 
     
+    return(phrase)
 
+def get_seed_from_phrase(phrase):
+    normalized_mnemonic = unicodedata.normalize("NFKD", phrase)
+    password = ""
+    normalized_passphrase = unicodedata.normalize("NFKD", password)
 
+    passphrase = "mnemonic" + normalized_passphrase
+    mnemonic = normalized_mnemonic.encode("utf-8")
+    passphrase = passphrase.encode("utf-8")
 
+    seed = hashlib.pbkdf2_hmac("sha512", mnemonic, passphrase, 2048)
+    print("\nThis is your BIP39 Seed :\n", binascii.hexlify(seed[:64]))
 
+    print("\nPlease note that this seed is a perfect match with the Mnemonic on this website:\nhttps://iancoleman.io/bip39/\n\n")
+
+    return(seed)
+    
+  
 
 if __name__ == "__main__":
 
-    print(f"Enter your seed or type rand to get a new one")
+    print(f"\nType enter to get a seed or type your's with spaces.\n")
     choice = input()
-    if choice == "rand":
-        seed = get_rand_seed()
+    if choice == "":
+        phrase = get_rand_phrase()
     else:
-        seed = get_seed()
+        phrase = get_phrase_from_user(choice)
+
+    seed = get_seed_from_phrase(phrase)
+    
+    
 
 
 
